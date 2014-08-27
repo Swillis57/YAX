0b6f74b1c60b1f1aec3ae8a51e7787f6feed1c88
Sam Willis <sw9662@g.rit.edu>
2014-08-27 12:49:42 -0400

    Fixed GLFW header include in GraphicsProfile.h

--- Framework/include/Graphics/GraphicsProfile.h
+++ Framework/include/Graphics/GraphicsProfile.h
@@ -1,11 +1,11 @@
 #ifndef GRAPHICS_PROFILE
 #define GRAPHICS_PROFILE
 
-#include <GLFW\glfw3.h>
+#include "../GLFW/glfw3.h"
 
 namespace XNA
 {
-    enum GraphicsProfile
+	enum GraphicsProfile
 	{			 
 		HiDef = GLFW_OPENGL_API,
 		Reach = GLFW_OPENGL_ES_API
