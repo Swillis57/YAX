a7fb1a5b2ab9ff892a098e48f7f2d7f8edd0f95c
Sam Willis <sw9662@g.rit.edu>
2014-09-03 13:50:46 -0400

    Tweaked BoundingFrustum.h

--- Framework/include/Geometry/BoundingFrustum.h
+++ Framework/include/Geometry/BoundingFrustum.h
@@ -4,19 +4,19 @@
 #include <array>
 #include "../Utils.h"
 #include "../Nullable.h"
-#include "Plane.h"
+#include "Matrix.h"
 
 namespace XNA
 {
-	struct Matrix;
+	struct Plane;
 	struct BoundingBox;
 	struct BoundingFrustum;
 	struct BoundingSphere;
 	struct Vector3;
 	struct Ray;
 	
-    enum ContainmentType;
-    enum PlaneIntersectionType;
+	enum ContainmentType;
+	enum PlaneIntersectionType;
 
 	struct BoundingFrustum
 	{
@@ -24,21 +24,21 @@ namespace XNA
 	
 		BoundingFrustum(const Matrix&);
 
+		XNA::Matrix Matrix;
+
 		Plane Bottom();
 		Plane Far();
 		Plane Left();
 		Plane Near();
 		Plane Right();
 		Plane Top();
-		XNA::Matrix Matrix();
-		void Matrix(const XNA::Matrix&);
-
+		
 		ContainmentType Contains(const BoundingBox&);
 		ContainmentType Contains(const BoundingFrustum&);
 		ContainmentType Contains(const BoundingSphere&);
 		ContainmentType Contains(const Vector3&);
 
-		std::array<Vector3, 8> GetCorners();
+		const std::array<Vector3, 8>& GetCorners();
 
 		bool Intersects(const BoundingBox&);
 		bool Intersects(const BoundingFrustum&);
@@ -48,6 +48,9 @@ namespace XNA
 
 		bool operator==(const BoundingFrustum&);
 		bool operator!=(const BoundingFrustum&);
+
+	private:
+		std::array<Vector3, 8> _corners;
 	};
 }
 
