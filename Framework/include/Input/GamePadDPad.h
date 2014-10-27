d9f33e82d83d3402e13da305ab3fa4d21a9bc84b
Sam Willis <sw9662@g.rit.edu>
2014-10-27 09:47:30 -0400

    Started GamePadDPad.h

---
+++ Framework/include/Input/GamePadDPad.h
@@ -0,0 +1,16 @@
+#ifndef _GAME_PAD_DPAD_H
+#define _GAME_PAD_DPAD_H
+
+namespace XNA
+{
+    struct GamePadDPad
+    {
+    public:
+        GamePadDPad(ButtonState, ButtonState, ButtonState, ButtonState);
+
+        
+    }
+}
+
+#endif
+
