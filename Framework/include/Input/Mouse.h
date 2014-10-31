98ac71d763326b68f60154cb71c048b3449195ea
Sam Willis <samwill01@optimum.net>
2014-10-31 02:02:05 -0400

    Finished mouse input

--- Framework/include/Input/Mouse.h
+++ Framework/include/Input/Mouse.h
@@ -1,17 +1,23 @@
 #ifndef _MOUSE_H
 #define _MOUSE_H
 
+#include "../Utils.h"
 
 struct GLFWwindow;
 
 namespace XNA
 {
+	class MouseState;
+
 	class Mouse
 	{
+	public:
 		static GLFWwindow* WindowHandle();
 		static void WindowHandle(GLFWwindow*);
 
-		
+		static MouseState GetState();
+
+		static void SetPosition(i32, i32);
 	};
 }
 
