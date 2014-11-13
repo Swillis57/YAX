601ab624d21ffed03b702d3ad5921f75e3b84d50
Sam Willis <samwill01@optimum.net>
2014-11-13 01:46:32 -0500

    Changed const-qualifiers on Set- and GetData functions in VertexBuffer

--- Framework/include/Graphics/VertexBuffer.h
+++ Framework/include/Graphics/VertexBuffer.h
@@ -26,13 +26,13 @@ namespace YAX
 		i32 VertexCount() const;
 		YAX::VertexDeclaration VertexDeclaration() const;
 
-		void GetData(i32, std::vector<YAX::VertexDeclaration>, i32, i32, i32) const;
-		void GetData(std::vector<YAX::VertexDeclaration>) const;
-		void GetData(std::vector<YAX::VertexDeclaration>, i32, i32) const;
+		void GetData(i32, std::vector<YAX::VertexDeclaration>&, i32, i32, i32) const;
+		void GetData(std::vector<YAX::VertexDeclaration>&) const;
+		void GetData(std::vector<YAX::VertexDeclaration>&, i32, i32) const;
 
-		void SetData(i32, std::vector<YAX::VertexDeclaration>, i32, i32, i32);
-		void SetDate(std::vector<YAX::VertexDeclaration>);
-		void SetData(std::vector<YAX::VertexDeclaration>, i32, i32);
+		void SetData(i32, const std::vector<YAX::VertexDeclaration>&, i32, i32, i32);
+		void SetDate(const std::vector<YAX::VertexDeclaration>&);
+		void SetData(const std::vector<YAX::VertexDeclaration>&, i32, i32);
 
 	private:
 		YAX::BufferUsage _usage;
