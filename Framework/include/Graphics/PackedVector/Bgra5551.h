2ea66e8f5e3f588af074e1afcdedebbacbee5996
Sam Willis <samwill01@optimum.net>
2015-01-10 21:27:02 -0500

    Finished Bgra5551

--- Framework/include/Graphics/PackedVector/Bgra5551.h
+++ Framework/include/Graphics/PackedVector/Bgra5551.h
@@ -6,15 +6,21 @@
 namespace YAX
 {
 	struct Bgra5551 : IPackedVector<ui16>
-	{
-		Bgra5551(float, float, float, float);
+	{						 
+		typedef IPackedVector<ui16> Base;
+
+		Bgra5551(float b, float g, float r, float a);
 		Bgra5551(const Vector4&);
+		~Bgra5551();
 
 		Vector4 ToVector4() const override;
 		void PackFromVector4(const Vector4&) override;
 
 		friend bool operator==(const Bgra5551&, const Bgra5551&);
 		friend bool operator!=(const Bgra5551&, const Bgra5551&);
+
+	private:
+		void Pack(float b, float g, float r, float a);
 	};
 }
 
