e187cce416621816893d533571707609bd9c9e14
Sam Willis <samwill01@optimum.net>
2015-01-16 14:38:01 -0500

    Updated main.cpp for testing

--- Framework/main.cpp
+++ Framework/main.cpp
@@ -1,5 +1,26 @@
 //For testing purposes
 
+#include <iostream>
+#include <cstdint>
+
+#include "include/GLEW/glew.h"
+#include "include/GLFW/glfw3.h"
+
+#include "include/Graphics/GraphicsAdapter.h"
+
 int main()
 {
+	if (!glewInit()) return -1;
+	if (!glfwInit()) return -1;
+
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	GLFWwindow* win = glfwCreateWindow(800, 600, "Test", NULL, NULL);
+	glfwMakeContextCurrent(win);
+
+	YAX::GraphicsAdapter::FindAdapters();
+	auto g = YAX::GraphicsAdapter::Adapters();
+
+	glfwTerminate();
 }
