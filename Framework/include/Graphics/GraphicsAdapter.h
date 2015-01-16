c5a0d48125e53bb87a960d734959d2cf4dba8f94
Sam Willis <samwill01@optimum.net>
2015-01-16 14:36:41 -0500

    Removing old code and tweaking

--- Framework/include/Graphics/GraphicsAdapter.h
+++ Framework/include/Graphics/GraphicsAdapter.h
@@ -16,23 +16,24 @@ namespace YAX
 	enum class GraphicsProfile : int;
 
 	typedef std::vector<DisplayMode> DisplayModeCollection;
-	//typedef std::tuple<SurfaceFormat, DepthFormat, int> BufferQueryResults;
 
+	//GraphicsAdapter encapsulates all of the information needed
+	//to identify monitors and the GPU they're connected to
 	class GraphicsAdapter
-	{
+	{	  
+		//Using the pimpl idiom to cut down on header includes
 		struct Impl;
 
 	public:
-		GraphicsAdapter(std::string description, std::string vendor, GLFWmonitor* handle);
+		GraphicsAdapter(std::string name, std::string description, std::string vendor, GLFWmonitor* handle);
 		~GraphicsAdapter();
 		GraphicsAdapter(const GraphicsAdapter&);
 		GraphicsAdapter(GraphicsAdapter&&);
-
 		GraphicsAdapter& operator=(const GraphicsAdapter&);
 		GraphicsAdapter& operator=(GraphicsAdapter&&);
 
 		static const std::vector<GraphicsAdapter>& Adapters();
-		static GraphicsAdapter* DefaultAdapter();
+		static GraphicsAdapter DefaultAdapter();
 		static void FindAdapters();
 		
 		
@@ -45,31 +46,12 @@ namespace YAX
 		DisplayMode CurrentDisplayMode() const;
 		const DisplayModeCollection& SupportedDisplayModes() const;
 		
-
-		/* Not implementing unless there's a demonstrable need for them
-		i32 DeviceID() const;
-		i32 Revision() const;
-		i32 SubSystemID() const;
-		*/
-		
-		
-		/*
-		Not going to implement these quite yet,
-		as I'm not sure they're entirely necessary (or make sense) in OpenGL
-		
-		bool QueryBackBufferFormat(GraphicsProfile, SurfaceFormat, DepthFormat, i32, BufferQueryResults&) const;
-		bool QueryRenderTargetFormat(GraphicsProfile, SurfaceFormat, DepthFormat, i32, BufferQueryResults&) const;
-		static bool UseNullDevice();
-		static void UseNullDevice(bool);
-		static bool UseReferenceDevice();
-		static void UseReferenceDevice(bool);
-		*/
 	
 	private:
 		std::unique_ptr<Impl> _impl;
 
 		static std::vector<GraphicsAdapter> _adapters;
-		static GraphicsAdapter* _defaultAdapter;
+		static GraphicsAdapter& _defaultAdapter;
 		static bool _useNull, _useRef;
 
 	};
