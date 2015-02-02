bd7a50ed3af1b8eb21f073ff2be9cdc96bb2ec23
unknown <sw9662@OH1370-27.main.ad.rit.edu>
2015-02-02 13:43:23 -0500

    Finished Matrix::CreateFromQuaternion

--- Framework/src/Math/Matrix.cpp
+++ Framework/src/Math/Matrix.cpp
@@ -163,6 +163,14 @@ namespace YAX
 
 	Matrix Matrix::CreateFromQuaternion(const Quaternion& q)
 	{
+		//variables for sanity
+		float x = q.X, y = q.Y, z = q.Z, w = q.W;
 
+		return Matrix(1-2*y*y-2*z*z,   2*x*y-2*z*w,   2*x*z+2*y*w,   0,
+					    2*x*y+2*z*w, 1-2*x*x-2*z*z,   2*y*z-2*x*w,   0,
+					    2*x*z-2*y*w,   2*y*z+2*x*w, 1-2*x*x-2*y*y,   0,
+								  0,			 0,		        0, 1.0f);
 	}
+
+
 }
