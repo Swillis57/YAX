036001be802086703b5638ae1a3c964fdc716b4c
Sam Willis <samwill01@optimum.net>
2015-02-09 03:08:16 -0500

    Started Quaternion implementation

---
+++ Framework/src/Math/Quaternion.cpp
@@ -0,0 +1,10 @@
+#include "../../include/Math/Quaternion.h"
+
+#include "../../include/Math/MathHelper.h" 
+#include "../../include/Math/Matrix.h"
+#include "../../include/Math/Vector3.h"
+
+namespace YAX
+{
+	const Quaternion Quaternion::Identity(0, 0, 0, 1.0f);
+}
