98a53c657ef571dfc7600a289fd75041290f1f59
Sam Willis <samwill01@optimum.net>
2015-02-09 03:07:33 -0500

    Quaternion header updates

--- Framework/include/Math/Quaternion.h
+++ Framework/include/Math/Quaternion.h
@@ -12,40 +12,46 @@ namespace YAX
 		float X, Y, Z, W;
 		static const Quaternion Identity;
 
-		Quaternion(float, float, float, float);
-		Quaternion(Vector3, float);
+		Quaternion(float x, float y, float z, float w);
+		Quaternion(Vector3 xyz, float w);
 
 		void Conjugate();
-		float Dot(const Quaternion&) const;
+		float Dot(const Quaternion& q) const;
 		float Length() const;
 		float LengthSquared() const;
 		void Normalize();
 		
-		static Quaternion Concatenate(const Quaternion&, const Quaternion&);
-		static Quaternion Conjugate(const Quaternion&);
-		static Quaternion CreateFromAxisAngle(Vector3, float);
-		static Quaternion CreateFromRotationMatrix(const Matrix&);
-		static Quaternion CreateFromYawPitchRoll(float, float, float);
-		static float Dot(const Quaternion&, const Quaternion&);
-		static Quaternion Inverse(const Quaternion&);
-		static Quaternion Lerp(const Quaternion&, const Quaternion&, float);
-		static Quaternion Normalize(const Quaternion&);
-		static Quaternion Slerp(const Quaternion&, const Quaternion&, float);
+		static Quaternion Concatenate(const Quaternion& first, const Quaternion& second);
+		static Quaternion Conjugate(Quaternion);
+		static Quaternion CreateFromAxisAngle(const Vector3& axis, float angle);
+		static Quaternion CreateFromRotationMatrix(const Matrix& mat);
+		static Quaternion CreateFromYawPitchRoll(float yaw, float pitch, float roll);
+		static float Dot(const Quaternion& q1, const Quaternion& q2);
+		static Quaternion Inverse(Quaternion q);
+		static Quaternion Lerp(const Quaternion& from, const Quaternion& to, float t);
+		static Quaternion Normalize(Quaternion q);
+		static Quaternion Slerp(const Quaternion& from, const Quaternion& to, float t);
 
-
-		Quaternion& operator=(const Quaternion&) const;
-		Quaternion operator+(const Quaternion&) const;
-		Quaternion operator-(const Quaternion&) const;
-		Quaternion operator*(const Quaternion&) const;
-		Quaternion operator*(float);
-		Quaternion operator/(const Quaternion&) const;
-		Quaternion operator/(float);
-		Quaternion operator-() const;
-		bool operator==(const Quaternion&) const;
-		bool operator!=(const Quaternion&) const;
-
+		Quaternion& operator+=(const Quaternion&);
+		Quaternion& operator-=(const Quaternion&);
+		Quaternion& operator*=(const Quaternion&);
+		Quaternion& operator*=(float);
+		Quaternion& operator/=(const Quaternion&);
+		Quaternion& operator/=(float);
 
 	};
+
+	Quaternion operator+(const Quaternion&, const Quaternion&);
+	Quaternion operator-(const Quaternion&, const Quaternion&);
+	Quaternion operator*(const Quaternion&, const Quaternion&);
+	Quaternion operator*(const Quaternion&, float);
+	Quaternion operator*(float, const Quaternion&);
+	Quaternion operator/(const Quaternion&, const Quaternion&);
+	Quaternion operator/(const Quaternion&, float);
+	Quaternion operator-(const Quaternion&);
+
+	bool operator==(const Quaternion&, const Quaternion&);
+	bool operator!=(const Quaternion&, const Quaternion&);
 }
 
 
