9a4dd09110c98e08e48706c6b1018d728f6dde0f
unknown <sw9662@GDD-8.main.ad.rit.edu>
2015-02-11 14:46:54 -0500

    Vector4 header updates

--- Framework/include/Math/Vector4.h
+++ Framework/include/Math/Vector4.h
@@ -17,50 +17,58 @@ namespace YAX
 		
 		float X, Y, Z, W;
 		
-		Vector4(float);
-		Vector4(float, float, float, float);
-		Vector4(Vector2, float, float);
-		Vector4(Vector3, float);
+		Vector4(float val);
+		Vector4(float x, float y, float z, float w);
+		Vector4(Vector2 xy, float z, float w);
+		Vector4(Vector3 xyz, float w);
 
-		static Vector4 Barycentric(const Vector4&, const Vector4&, const Vector4&, float, float);
-		static Vector4 CatmullRom(const Vector4&, const Vector4&, const Vector4&, float);
-		static Vector4 Clamp(const Vector4&, const Vector4&, const Vector4&);
-		static Vector4 Distance(const Vector4&, const Vector4&);
-		static Vector4 DistanceSquared(const Vector4&, const Vector4&);
-		static Vector4 Dot(const Vector4&, const Vector4&);
-		static Vector4 Hermite(const Vector4&, const Vector4&, const Vector4&, const Vector4&, float);
-		static Vector4 Max(const Vector4&, const Vector4&);
-		static Vector4 Min(const Vector4&, const Vector4&);
-		static Vector4 Reflect(const Vector4&, const Vector4&);
-		static Vector4 SmoothStep(const Vector4&, const Vector4&, float);
+		static Vector4 Barycentric(const Vector4& p1, const Vector4& p2, const Vector4& p3, float b2, float b3);
+		static Vector4 CatmullRom(const Vector4& p1, const Vector4& p2, const Vector4& p3, const Vector4& p4, float amt);
+		static Vector4 Clamp(const Vector4& val, const Vector4& min, const Vector4& max);
+		static Vector4 Distance(const Vector4& p1, const Vector4& p2);
+		static Vector4 DistanceSquared(const Vector4& p1, const Vector4& p2);
+		static Vector4 Dot(const Vector4& v1, const Vector4& v2);
+		static Vector4 Hermite(const Vector4& p1, const Vector4& t1, const Vector4& p2, const Vector4& t2, float amt);
+		static Vector4 Lerp(const Vector4& from, const Vector4& to, float t);
+		static Vector4 Max(const Vector4& v1, const Vector4& v2);
+		static Vector4 Min(const Vector4& v1, const Vector4& v2);
+		static Vector4 SmoothStep(const Vector4& from, const Vector4& to, float t);
 
-		static Vector4 Transform(const Vector4&, const Matrix&);
-		static Vector4 Transform(const Vector4&, const Quaternion&);
-		static void Transform(const std::vector<Vector4>&, i32, const Matrix&, std::vector<Vector4>&, i32, i32);
-		static void Transform(const std::vector<Vector4>&, i32, const Quaternion&, std::vector<Vector4>&, i32, i32);
-		static void Transform(const std::vector<Vector4>&, const Matrix&, std::vector<Vector4>&);
-		static void Transform(const std::vector<Vector4>&, const Quaternion&, std::vector<Vector4>&);
+		static Vector4 Transform(const Vector4& vec, const Matrix& mat);
+		static Vector4 Transform(const Vector4& vec, const Quaternion& q);
+		static void Transform(const std::vector<Vector4>& source, i32 sourceIdx, const Matrix& mat, std::vector<Vector4>& dest, i32 destIdx, i32 count);
+		static void Transform(const std::vector<Vector4>& source, i32 sourceIdx, const Quaternion& q, std::vector<Vector4>& dest, i32 destIdx, i32 count);
+		static void Transform(const std::vector<Vector4>& source, const Matrix& mat, std::vector<Vector4>& dest);
+		static void Transform(const std::vector<Vector4>& source, const Quaternion& q, std::vector<Vector4>& dest);
 
-		static Vector4 TransformNormal(const Vector4&, const Matrix&);
-		static void TransformNormal(const std::vector<Vector4>&, i32, const Matrix&, std::vector<Vector4>&, i32, i32);
-		static void TransformNormal(const std::vector<Vector4>&, const Matrix&, std::vector<Vector4>&);
+		static Vector4 TransformNormal(const Vector4& norm, const Matrix& mat);
+		static void TransformNormal(const std::vector<Vector4>& source, i32 sourceIdx, const Matrix& mat, std::vector<Vector4>& dest, i32 destIdx, i32 count);
+		static void TransformNormal(const std::vector<Vector4>& source, const Matrix& mat, std::vector<Vector4>& dest);
 
 		Vector4 Normalize();
 		float Length();
 		float LengthSquared();
 
-		Vector4 operator+(const Vector4&);
-		Vector4 operator-(const Vector4&);
-		Vector4 operator*(const Vector4&);
-		Vector4 operator*(float);
-		Vector4 operator/(const Vector4&);
-		Vector4 operator/(float);
-		Vector4 operator-();
-
-		bool operator==(const Vector4&);
-		bool operator!=(const Vector4&);
+		Vector4& operator+=(const Vector4&);
+		Vector4& operator-=(const Vector4&);
+		Vector4& operator*=(const Vector4&);
+		Vector4& operator*=(float);
+		Vector4& operator/=(const Vector4&);
+		Vector4& operator/=(float);
 
 	};
+
+	Vector4 operator+(const Vector4&, const Vector4&);
+	Vector4 operator-(const Vector4&, const Vector4&);
+	Vector4 operator*(const Vector4&, const Vector4&);
+	Vector4 operator*(float, const Vector4&);
+	Vector4 operator*(const Vector4&, float);
+	Vector4 operator/(const Vector4&, const Vector4&);
+	Vector4 operator/(float, const Vector4&);
+	Vector4 operator-(const Vector4&);
+
+	bool operator==(const Vector4&, const Vector4&);
+	bool operator!=(const Vector4&, const Vector4&);
 }
 
 
