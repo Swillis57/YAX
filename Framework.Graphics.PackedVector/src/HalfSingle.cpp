85230f3c92abc334e4b9f55b8c825cbd0edde550
Sam Willis <samwill01@optimum.net>
2015-02-21 03:04:37 -0500

    HalfSingle now packs and unpacks floats properly

--- Framework.Graphics.PackedVector/src/HalfSingle.cpp
+++ Framework.Graphics.PackedVector/src/HalfSingle.cpp
@@ -36,33 +36,68 @@ namespace YAX
 		return !(lhs == rhs);
 	}
 
+	
 	ui16 HalfSingle::Pack(float val)
 	{
 		ui16 res = 0;
 
+		//Retrieve float components
 		ui32 bits = BitCast<ui32>(val);
-		byte sign = bits & (1 << 31);
-		byte expoSign = bits & (1 << 30);
-		byte expo = bits & (0xF << 22);
-		ui16 mantissa = bits & (0x3FF << 12);
+		ui32 mantissa = READBITS_STATIC(bits, 0x7FFFFF, 0);
+		i32 expoBits = static_cast<i32>(READBITS_STATIC(bits, 0xFF, 23));
+		byte signBit = static_cast<byte>(READBITS_STATIC(bits, 0x1, 31));
+		byte expoSign = static_cast<byte>(READBITS_STATIC(expoBits, 0x1, 7));
 
-		res ^= res;
-		WRITEBITS_STATIC(res, sign, 1);
+		//Rebias exponent to 15-bit range
+		expoBits -= 112;
+
+		//If the new exponent is between1 and 30, it's a valid 16-bit expo
+		//Else if it's greater than 31, return infinity
+		//Else it's subnormal, so zero it out (may support this later
+		if (expoBits >= 1 && expoBits <= 30)
+		{
+			mantissa >>= 12;
+		}
+		else if (expoBits >= 31)
+		{
+			expoSign = 1;
+			expoBits = 0xFF;
+			mantissa = 0;
+		}
+		else
+		{
+			expoSign = 0;
+			expoBits = 0;
+			mantissa = 0;
+		}
+
+		WRITEBITS_STATIC(res, signBit, 1);
 		WRITEBITS_STATIC(res, expoSign, 4);
-		WRITEBITS_STATIC(res, expo, 10);
+		WRITEBITS_STATIC(res, (expoBits & 0xF), 10);
 		WRITEBITS_STATIC(res, mantissa, 0);
 
 		return res;
 	}
 
+	
 	float HalfSingle::Unpack(ui16 source)
 	{
 		ui32 res = 0;
 
-		WRITEBITS_STATIC(res, READBITS_STATIC(source, 0x1, 15), 1);
-		WRITEBITS_STATIC(res, READBITS_STATIC(source, 0x1, 14), 7);
-		WRITEBITS_STATIC(res, READBITS_STATIC(source, 0xF, 10), 10);
-		WRITEBITS_STATIC(res, READBITS_STATIC(source, 0x3FF, 0), 13);
+		//Pull the bits out of the 16-bit float
+		ui32 mantissa = READBITS_STATIC(source, 0x3FF, 0);
+		i32 expoBits = static_cast<i32>(READBITS_STATIC(source, 0x1F, 10));
+		byte signBit = static_cast<byte>(READBITS_STATIC(source, 0x1, 15));
+
+		//Rebias into 32-bit range
+		expoBits += 112;
+
+		//Shift the mantissa to make room for 23 bits
+		mantissa <<= 13;
+
+		WRITEBITS_STATIC(res, signBit, 8);
+		WRITEBITS_STATIC(res, expoBits, 23);
+		WRITEBITS_STATIC(res, mantissa, 0);
 
 		return BitCast<float>(res);
 	}
