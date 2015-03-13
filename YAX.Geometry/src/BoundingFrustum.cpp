9b49eeb723a1b4a760ce5d3a996db9771131449c
Sam Willis <samwill01@optimum.net>
2015-03-13 03:16:57 -0400

    Added defines for plane array access

--- YAX.Geometry/src/BoundingFrustum.cpp
+++ YAX.Geometry/src/BoundingFrustum.cpp
@@ -9,6 +9,12 @@
 #include "../../YAX.Math/include/Vector3.h"
 #include "../../YAX.Math/include/Vector4.h"
 
+#define TOP 0
+#define BOTTOM 1
+#define LEFT 2
+#define RIGHT 3
+#define FRONT 4
+#define BACK 5
 
 
 namespace YAX
