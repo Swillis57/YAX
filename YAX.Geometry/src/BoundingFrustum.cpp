6952fbd8f023f36a7db6b21bfcbb778e74afac3e
Sam Willis <samwill01@optimum.net>
2015-03-14 05:45:28 -0400

    Renaming and adding new methods for getter/setter and corners

--- YAX.Geometry/src/BoundingFrustum.cpp
+++ YAX.Geometry/src/BoundingFrustum.cpp
@@ -20,49 +20,58 @@
 namespace YAX
 {
 	BoundingFrustum::BoundingFrustum(YAX::Matrix m)
-		: _oldMat(m), Matrix(m)
+		: _mat(m)
 	{
 		updatePlanes();
+		updateCorners();
+	}
+
+	YAX::Matrix& BoundingFrustum::Matrix()
+	{
+		return _mat;
+	}
+
+	void BoundingFrustum::Matrix(YAX::Matrix m)
+	{
+		_mat = m;
+		updatePlanes();
+		updateCorners();
 	}
 
 	void BoundingFrustum::updatePlanes()
 	{
-		//Matrix hasn't changed, no update needed
-		if (Matrix == _oldMat) return;
-
-		_oldMat = Matrix;
 		//Store the plane equation coefficients in an array to
 		//make handling easier
 		Vector4 n[6];
-		n[TOP] = { Matrix.M14 - Matrix.M12,
-				   Matrix.M24 - Matrix.M22,
-				   Matrix.M34 - Matrix.M32,
-				   Matrix.M44 - Matrix.M42 };
+		n[TOP] = { _mat.M14 - _mat.M12,
+				   _mat.M24 - _mat.M22,
+				   _mat.M34 - _mat.M32,
+				   _mat.M44 - _mat.M42 };
 
-		n[BOTTOM] = { Matrix.M12 + Matrix.M14,
-					  Matrix.M22 + Matrix.M24,
-					  Matrix.M32 + Matrix.M34,
-					  Matrix.M42 + Matrix.M44 };
+		n[BOTTOM] = { _mat.M12 + _mat.M14,
+					  _mat.M22 + _mat.M24,
+					  _mat.M32 + _mat.M34,
+					  _mat.M42 + _mat.M44 };
 
-		n[LEFT] = { Matrix.M14 + Matrix.M11,
-					Matrix.M24 + Matrix.M21,
-					Matrix.M34 + Matrix.M31,
-					Matrix.M44 + Matrix.M41 };
+		n[LEFT] = { _mat.M14 + _mat.M11,
+					_mat.M24 + _mat.M21,
+					_mat.M34 + _mat.M31,
+					_mat.M44 + _mat.M41 };
 
-		n[RIGHT] = { Matrix.M14 - Matrix.M11,
-					 Matrix.M24 - Matrix.M21,
-					 Matrix.M34 - Matrix.M31,
-					 Matrix.M44 - Matrix.M41 };
+		n[RIGHT] = { _mat.M14 - _mat.M11,
+					 _mat.M24 - _mat.M21,
+					 _mat.M34 - _mat.M31,
+					 _mat.M44 - _mat.M41 };
 
-		n[FRONT] = { Matrix.M13,
-					 Matrix.M23,
-					 Matrix.M33,
-					 Matrix.M43 };
+		n[FRONT] = { _mat.M13,
+					 _mat.M23,
+					 _mat.M33,
+					 _mat.M43 };
 
-		n[BACK] = { Matrix.M14 - Matrix.M13,
-					Matrix.M24 - Matrix.M23,
-					Matrix.M34 - Matrix.M33,
-					Matrix.M44 - Matrix.M43 };
+		n[BACK] = { _mat.M14 - _mat.M13,
+					_mat.M24 - _mat.M23,
+					_mat.M34 - _mat.M33,
+					_mat.M44 - _mat.M43 };
 
 		for (int i = 0; i < 6; i++)
 		{
@@ -74,6 +83,10 @@ namespace YAX
 			v /= len;
 			_planes[i] = Plane({ v.X, v.Y, v.Z }, v.W);
 		}
+	}
+
+	void BoundingFrustum::updateCorners()
+	{
 
 	}
 
