f98c005fed59e51278ea1a3580c9df6d974040ef
Sam Willis <samwill01@optimum.net>
2015-03-16 23:45:13 -0400

    Finished BoundingSphere implementation. * TODO: See if there's a way to remove some sqrts from the Ray intersection function

--- YAX.Geometry/src/BoundingSphere.cpp
+++ YAX.Geometry/src/BoundingSphere.cpp
@@ -89,3 +89,58 @@ namespace YAX
 		else
 			return (cDot > 0 ? PlaneIntersectionType::Front : PlaneIntersectionType::Back);
 	}
+
+	std::unique_ptr<float> BoundingSphere::Intersects(const Ray& r) const
+	{
+		Vector3 rayToSphere = Center - r.Pos;
+		float l = Vector3::Dot(r.Dir, rayToSphere);
+		Vector3 proj = r.Dir * l;
+
+		//If the center of the sphere is behind the ray
+		if (l < 0)
+		{
+			float dist = rayToSphere.LengthSquared();
+
+			//If the ray's origin is outside the sphere, no collision
+			//Else if it's on the radius, return 0 dist
+			//Else it's inside the sphere so find the point of intersection
+			if (dist > Radius * Radius)
+				return std::make_unique<float>(nullptr);
+			else if (dist == Radius)
+				return std::make_unique<float>(0.0f);
+			else
+			{
+				float d = std::sqrtf(Radius*Radius - (proj - Center).LengthSquared());
+				float t = d - (proj - r.Pos).Length();
+				return std::make_unique<float>(t);
+			}
+		}
+		else //The center of the sphere is in front of the ray 
+		{
+			//If the center's projection onto the ray is outside of the radius, no collision
+			//Else find the point of contact closest to the ray's origin
+			if ((proj - Center).LengthSquared() > Radius*Radius)
+				return std::make_unique<float>(nullptr);
+			else
+			{
+				float d = std::sqrtf(Radius*Radius - (proj - Center).LengthSquared());
+				float t = (proj - r.Pos).Length();
+				t += (l > Radius*Radius ? -d : d);
+				return std::make_unique<float>(t);
+			}
+		}
+	}
+
+	BoundingSphere BoundingSphere::Transform(const Matrix& m) const
+	{
+		Vector3 center = Vector3::Transform(Center, m);
+		float scale = std::sqrtf(m.M11*m.M11 + m.M12*m.M12 + m.M13*m.M13);
+
+		return BoundingSphere(center, scale);
+	}
+
+	bool operator==(const BoundingSphere& lhs, const BoundingSphere& rhs)
+	{
+		return lhs.Center == rhs.Center && lhs.Radius == rhs.Radius;
+	}
+}
