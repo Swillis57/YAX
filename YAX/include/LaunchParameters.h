3c539c8ff94afa2168683fbe497cbc9c145f290b
Sam Willis <samwill01@optimum.net>
2015-03-18 03:43:54 -0400

    Switched LaunchParameters' typedef from a map to unordered_map, it doesn't need to be sorted

--- YAX/include/LaunchParameters.h
+++ YAX/include/LaunchParameters.h
@@ -1,12 +1,12 @@
 #ifndef _LAUNCH_PARAMETERS_H
 #define _LAUNCH_PARAMETERS_H
 
-#include <map>
+#include <unordered_map>
 #include <string>
 
 namespace YAX
 {
-	typedef std::map<std::string, std::string> LaunchParameters;
+	typedef std::unordered_map<std::string, std::string> LaunchParameters;
 }
 
 #endif
