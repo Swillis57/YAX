b615005df11988c3d9943e809fba6d427661749f
Sam Willis <samwill01@optimum.net>
2015-03-27 09:59:36 -0400

    Where XNA returned a reference, YAX will return a pointer. Also formatting

--- YAX/include/Game.h
+++ YAX/include/Game.h
@@ -20,7 +20,7 @@ namespace YAX
 		ContentManager& Content();
 		void Content(const ContentManager&);*/
 		
-		YAX::GraphicsDevice* GraphicsDevice();
+		YAX::GraphicsDevice& GraphicsDevice();
 
 		TimeSpan& InactiveSleepTime();
 		void InactiveSleepTime(const TimeSpan&);
@@ -34,49 +34,32 @@ namespace YAX
 		bool IsMouseVisible();
 		void IsMouseVisible(bool);
 
-		YAX::LaunchParameters& LaunchParameters();
+		YAX::LaunchParameters* LaunchParameters();
 		void LaunchParameters(const YAX::LaunchParameters&);
 
-		TimeSpan& TargetElapsedTime();
+		TimeSpan* TargetElapsedTime();
 		void TargetElapsedTime(const TimeSpan&);
 
-		GameWindow Window();
+		GameWindow* Window();
 		void Window(GameWindow);
 
-
 		void Exit();
-
 		void ResetElapsedTime();
-
 		void Run();
-
 		void RunOneFrame();
-
 		void SuppressDraw();
-
 		void Tick();
 
 	protected:
 		virtual void BeginDraw();
-
 		virtual void BeginRun();
-
 		virtual void Draw();
-
 		virtual void EndDraw();
-
 		virtual void EndRun();
-
 		virtual void Initialize();
-
 		virtual void LoadContent();
-
 		virtual void UnloadContent();
-
 		virtual void Update();
-
-
-
 	};
 }
 #endif
