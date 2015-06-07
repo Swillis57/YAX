348517c95998813d323a37bf23f5d4b0033a7768
Sam Willis <samwill01@optimum.net>
2015-06-07 02:30:54 -0400

    Changed EffectParameterCollection to an unordered_map

--- YAX/include/Graphics/EffectParameterCollection.h
+++ YAX/include/Graphics/EffectParameterCollection.h
@@ -1,13 +1,13 @@
 #ifndef _EFFECT_PARAMETER_COLLECTION_H
 #define _EFFECT_PARAMETER_COLLECTION_H
 
-#include <map>
+#include <unordered_map>
 #include <string>
 #include "EffectParameter.h"
 
 namespace YAX
 {
-	typedef std::map<std::string, EffectParameter> EffectParameterCollection;
+	typedef std::unordered_map<std::string, EffectParameter> EffectParameterCollection;
 }
 
 
